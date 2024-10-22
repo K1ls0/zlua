8ed0281c25d364289cfab24c192089a5c1d2e3f8
Paul Seydel <Seydel.fnvd@gmail.com>
2024-10-22 16:51:28 +0200

    Added module to build.zig

--- build.zig
+++ build.zig
@@ -4,6 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    // Lua c dependency
     const luac_dep = b.dependency("luac", .{
         .target = target,
         .optimize = optimize,
@@ -13,7 +14,6 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-
     luac_lib.addCSourceFiles(.{
         .root = luac_dep.path("src"),
         .files = c_files,
@@ -23,6 +23,7 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(luac_lib);
 
+    // zlua library
     const lib = b.addStaticLibrary(.{
         .name = "zlua",
         .root_source_file = b.path("src/root.zig"),
@@ -32,17 +33,11 @@ pub fn build(b: *std.Build) void {
     lib.linkLibrary(luac_lib);
     b.installArtifact(lib);
 
-    // Add lua files
-    //{
-    //    lib.linkLibC();
-    //    lib.addIncludePath(b.path(lib_files_dir));
-    //    inline for (c_files) |cfile| {
-    //        lib.addCSourceFile(.{
-    //            .file = b.path(lib_files_dir ++ cfile),
-    //            .flags = &.{},
-    //        });
-    //    }
-    //}
+    _ = b.addModule("zlua", .{
+        .target = target,
+        .optimize = optimize,
+        .root_source_file = b.path("src/root.zig"),
+    });
 
     const lib_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
@@ -52,41 +47,36 @@ pub fn build(b: *std.Build) void {
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     test_step.dependOn(&run_lib_unit_tests.step);
 
-    {
-        const exe = b.addExecutable(.{
-            .name = "zig_lua",
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        });
-        exe.step.dependOn(&lib.step);
-        exe.root_module.addImport("zlua", &lib.root_module);
-        b.installArtifact(exe);
-        const run_cmd = b.addRunArtifact(exe);
+    // Executable
+    const exe = b.addExecutable(.{
+        .name = "zig_lua",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.step.dependOn(&lib.step);
+    exe.root_module.addImport("zlua", &lib.root_module);
+    b.installArtifact(exe);
+    const run_cmd = b.addRunArtifact(exe);
 
-        run_cmd.step.dependOn(b.getInstallStep());
-        if (b.args) |args| {
-            run_cmd.addArgs(args);
-        }
-
-        const run_step = b.step("run", "Run the app");
-        run_step.dependOn(&run_cmd.step);
-
-        const exe_unit_tests = b.addTest(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        });
-        const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-        test_step.dependOn(&run_exe_unit_tests.step);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
     }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    const exe_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    test_step.dependOn(&run_exe_unit_tests.step);
 }
-//const lib_files_dir = "./libs/lua/lua-5.4.6/src/";
 
 const c_files: []const []const u8 = &.{
     "ldo.c",
